df188b040653bc7f7667ff77bbbf779bd02f116b
Robert0Sil <sil_rober@hotmail.es>
2018-08-04 22:36:56 -0500

    se termina el ejercicio 2

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const ejs = require('ejs')
+
 const pageRouter = require('./src/routes/pageRouter.js')
 const apiRouter = require('./src/routes/apiRouter.js')
 
 const app = express()
 const PORT = 3000
 
+app.use(express.static(`${__dirname}/public`))
+
+app.engine('ejs', ejs.renderFile)
+app.set('view engine', 'ejs')
+app.set('views', `${__dirname}/src/views`)
 app.use('/api', apiRouter)
 app.use('/', pageRouter)
 
--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -4,25 +4,103 @@ const apiRouter = Router()
 
 apiRouter.get('/', (req, res)=>{
   res.json({
-    '/api/users': 'Show users',
-    '/api/messages': 'Show messages'
+    '/api/jobs': 'Show Jobs',
+    '/api/jobsData' : 'Show Data Jobs',
+    '/api/companies': 'Show Companies',
+    '/api/companiesData': 'Show Data Companies'
   })
 })
 
-apiRouter.get('/users', (req, res)=>{
+apiRouter.get('/jobs', (req, res)=>{
   res.json([
-    {id: 1, name: 'Steffi', age: 25},
-    {id: 2, name: 'Adrian', age: 28},
-    {id: 3, name: 'Bertha', age: 77}
+    {
+      "title": 'SQL Server Administrator - Postgres',
+      "description": 'Bring to the table win-win survival strategies to ensure proactive domination. User generated "content" in real-time will have multiple touchpoints for offshoring.',
+      "location": 'Guadalajara',
+      "salary": 27000,
+      "fullTime": true,
+      "companyId": 1
+    },
+    {
+      "title": 'UX Engineer',
+      "description": 'Override the digital divide with additional clickthroughs from DevOps. Leverage agile frameworks to provide a robust synopsis for high level overviews.',
+      "location": 'Monterrey',
+      "salary": 35000,
+      "fullTime": true,
+      "companyId": 1
+    }
+
   ])
 })
 
-apiRouter.get('/messages', (req, res)=>{
+apiRouter.get('/companies', (req, res)=>{
   res.json([
-    { id: 1, user_id: 2, body: 'Hey Bertha!'},
-    { id: 2, user_id: 3, body: 'Hey Steffi!'},
-    { id: 3, user_id: 2, body: 'Good to see you'}
+    {
+      "name": 'Company ABC',
+      "description": 'Energistically network alternative technology deploying impactful partnerships.',
+      "imageLink": 'http://www.tinygraphs.com/labs/isogrids/hexa16/nsuaio',
+      "location": 'Guadalajara'
+    },
+    {
+      "name": 'Lossless Enterprises',
+      "description": 'Quickly strategizing team driven "outside the box" thinking.',
+      "location": 'Ciudad de Mexico',
+      "imageLink": 'http://www.tinygraphs.com/labs/isogrids/hexa16/8282',
+    }
   ])
 })
 
+apiRouter.get('/jobsData', (req, res)=>{
+  res.json([
+    {
+      title: 'SQL Server Administrator - Postgres',
+      description: 'Bring to the table win-win survival strategies to ensure proactive domination. User generated content in real-time will have multiple touchpoints for offshoring.',
+      location: 'Guadalajara',
+      salary: 27000,
+      fullTime: true,
+      companyId: 1
+    },
+    {
+      title: 'UX Engineer',
+      description: 'Override the digital divide with additional clickthroughs from DevOps. Leverage agile frameworks to provide a robust synopsis for high level overviews.',
+      location: 'Monterrey',
+      salary: 35000,
+      fullTime: true,
+      companyId: 1
+    },
+    {
+      title: 'API Architect',
+      description: 'Collaboratively administrate turnkey channels whereas virtual e-tailers. Objectively seize scalable metrics whereas proactive e-services.',
+      location: 'Ciudad de Mexio',
+      salary: 39000,
+      fullTime: true,
+      companyId: 2
+    },
+    {
+      title: 'Mid-Level Front End Engineer',
+      description: 'Interactively coordinate proactive e-commerce via process-centric "outside the box" thinking. Completely pursue scalable customer service through sustainable potentialities.',
+      location: 'Ciudad de Mexico',
+      salary: 21000,
+      fullTime: false,
+      companyId: 2
+    }
+  ])
+})
+
+apiRouter.get('/companiesdata', (req, res)=>{
+  res.json([
+    {
+      name: 'Company ABC',
+      description: 'Energistically network alternative technology deploying impactful partnerships.',
+      imageLink: 'http://www.tinygraphs.com/labs/isogrids/hexa16/nsuaio',
+      location: 'Guadalajara'
+    },
+    {
+      name: 'Lossless Enterprises',
+      description: 'Quickly strategizing team driven "outside the box" thinking.',
+      location: 'Ciudad de Mexico',
+      imageLink: 'http://www.tinygraphs.com/labs/isogrids/hexa16/8282',
+    }
+  ])
+})
 module.exports = apiRouter

630059000cd30ae91fb75c05680a21582467b5ec
Robert0Sil <sil_rober@hotmail.es>
2018-08-04 22:01:16 -0500

    Se termina el ejercicio 1 de la parte 0

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const pageRouter = require('./src/routes/pageRouter.js')
+const apiRouter = require('./src/routes/apiRouter.js')
+
+const app = express()
+const PORT = 3000
+
+app.use('/api', apiRouter)
+app.use('/', pageRouter)
+
+app.use((req, res)=>{
+  res.send('<h3> 404 - big error - page no exist</h3>')
+})
+
+app.listen(PORT, ()=>{
+  console.log(`App listening on localhost:${PORT}`);
+})
